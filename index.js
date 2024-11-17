ab341c67eed3ac9b1048a036b326f21e6aaa63ce
douglasvcv <douglasvitor1233@gmail.com>
2024-11-17 17:21:36 -0300

    finish the script

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-let input = String( document.getElementById("cepInput"))
+let input = document.getElementById("cepInput")
 let button = document.getElementById("buttonCLick")
 let span = document.getElementById("responseSpan")
 
-button.addEventListener('click',async (event)=>{
- let response = await Cep(input)
- console.log(response)
-  span.innerText = `${{response}}`
-})
 
 async function Cep(cep) {
-  await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then((response) => {
-      if (!response.ok) {
-       throw new Error("Resposta da rede não foi bem-sucedida")
-      }
-     return  response.json();
-    })
-    .then(
-        data=>{
-            console.log(data)
-        }
-    )
-    .catch(
-        (error) => {
-      console.log(`Erro na requisição: ${error}`);
-    });
-}
-
+  try{
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    if(!response.ok){
+      throw new Error("Resposta da rede não foi bem-sucedida")
+    }
+    const data = await response.json()
+    return data
+  }catch(error){
+    console.log(`Erro na requisição ${error}`)
+    return null
+  }
+  }
+  
+  button.addEventListener('click',async (event)=>{
+   let response = await Cep(input.value)
+    if(response){
+      console.log(response.logradouro)
+      span.innerText= `A rua é: ${response.logradouro}`
+    }else{
+      span.innerText = `Endereço não encontrado. Por favor, verificar o CEP`
+    }
+  })
+ 
